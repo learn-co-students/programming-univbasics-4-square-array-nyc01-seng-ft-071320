708d6bb03b3282d0f5d9015b8a6edd22282eb58e
Vladilen Napuri <vladnapuri@gmail.com>
2020-06-25 20:16:21 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  counter = 0
+  array.length.times do |counter|
+    new_array << (array[counter] ** 2)
+    counter += 1
+  end
+  new_array
 end
