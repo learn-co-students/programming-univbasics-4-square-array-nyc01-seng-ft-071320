089586672d7f21db046cb6528a5025721b9372e0
adept-bell-4492 <garlandegoma@gmail.com>
2020-07-03 01:59:31 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
+
 def square_array(array)
+  count= 0 
+  new_array= [ ]
+  while count < array.length do
+    new_array << array[count]** 2
+    count += 1 
+  end
+  new_array
+    
   # your code here
 end
