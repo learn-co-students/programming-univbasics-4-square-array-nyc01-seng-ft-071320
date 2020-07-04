273b4572ac4be16c9501610cd460b6ea144e1538
Israel Canessa <israellcanessa@gmail.com>
2020-07-04 08:30:59 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
+require 'pry'
+
+array = [1,2,3]
+
 def square_array(array)
-  # your code here
+  new_array = []
+  array.length.times { |index|
+    new_array.push(array[index] ** 2) }
+new_array
 end
